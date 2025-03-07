42e4125051b4e89e37d17682d4c6919f9a1ac415
Vinh1222001 <nguyentuanvinh1222@gmail.com>
2025-03-07 11:24:25 +0700

    Refactor: main file

---
+++ include/main.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <Arduino.h>
+
+#include "line_follower.hpp"
+#include "line_color_tracker.hpp"
+#include "shift_register.hpp"
+#include "motor_driver.hpp"
+#include "mpu_reader.hpp"
+#include "ultra_sonic_reader.hpp"
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
-#include <Arduino.h>
-#include "line_follower.hpp"
-#include "line_color_tracker.hpp"
-#include "shift_register.hpp"
-#include "motor_driver.hpp"
-#include "mpu_reader.hpp"
-#include "ultra_sonic_reader.hpp"
+#include "main.hpp"
 
 void setup()
 {
